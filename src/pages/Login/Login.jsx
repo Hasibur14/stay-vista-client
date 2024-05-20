8014b3d05c0911cdb24d6b2423e5d48efaed46d9
hasibur14 <hasib141415@gmail.com>
2024-05-20 19:50:13 +0600

    added user signIn in  firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,47 @@
-import { Link } from 'react-router-dom'
+import toast from 'react-hot-toast'
 import { FcGoogle } from 'react-icons/fc'
+import { Link, useNavigate } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Login = () => {
+
+
+  const navigate = useNavigate()
+  const { signInWithGoogle, signIn, loading, setLoading } = useAuth()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const form = e.target
+    const email = form.email.value
+    const password = form.password.value
+
+    try {
+      setLoading(true)
+
+      await signIn(email, password)
+      navigate('/')
+      toast.success('Sign in successfully')
+
+    } catch (err) {
+      toast.error(err.message)
+    }
+  };
+
+  //google sign in
+  const handleGoogleSignIn = async () => {
+
+    try {
+      await signInWithGoogle()
+      navigate('/')
+      toast.success('Sign in successfully')
+
+    } catch (err) {
+      toast.error(err.message)
+    }
+  };
+
+
+
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -12,6 +52,7 @@ const Login = () => {
           </p>
         </div>
         <form
+          onSubmit={handleSubmit}
           noValidate=''
           action=''
           className='space-y-6 ng-untouched ng-pristine ng-valid'
@@ -70,11 +111,14 @@ const Login = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button
+          disabled={loading}
+          onClick={handleGoogleSignIn}
+          className='disabled:cursor-not-allowed flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className='px-6 text-sm text-center text-gray-400'>
           Don&apos;t have an account yet?{' '}
           <Link
